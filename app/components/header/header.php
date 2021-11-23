68475f3593e9e375b82c47e5bde6b4e0093ad75e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-11-23 02:47:18 +0200

    Header. Part 1

---
+++ app/components/header/header.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <div class="header__inner">
+        <a href="/" class="header__logo">Y4T6 Project</a>
+        <div class="header__description">
+            <div class="header__description-row">the</div>
+            <div class="header__description-row">new way</div>
+            <div class="header__description-row">of everything</div>
+        <div>
+    </div>
+</header>
