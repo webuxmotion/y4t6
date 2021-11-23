d9f3595ed50c080c5b35aa16cd6a08ff3cb1b01d
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-11-24 01:29:44 +0200

    Hero block

--- app/components/hero/hero.php
+++ app/components/hero/hero.php
@@ -1,3 +1,13 @@
 <div class="hero">
-  <img src="/screen-placeholder.jpg" />
+  <div class="hero__inner">
+    <div class="hero__images">
+      <img src="/projects/otmn/otmn-v1-0-micro.jpg" />
+      <img src="/projects/otmn/otmn-v1-0-macro.jpg" />
+    </div>
+    <div class="hero__content">
+      <span class="hero__vertical-text">PROJECT</span>
+      <a href="/" class="hero__button">See details</a>
+      <h2 class="hero__big-text">OTMN V1.0</h2>
+    </div>
+  </div>
 </div>
