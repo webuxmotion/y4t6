ae8a6bb03302b4231d35126a29d19704ff7a6527
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-11-23 04:40:41 +0200

    Header

--- app/components/header/header.php
+++ app/components/header/header.php
@@ -1,10 +1,18 @@
 <header class="header">
     <div class="header__inner">
-        <a href="/" class="header__logo">Y4T6 Project</a>
-        <div class="header__description">
-            <div class="header__description-row">the</div>
-            <div class="header__description-row">new way</div>
-            <div class="header__description-row">of everything</div>
-        <div>
+        <div class="header__column">
+            <a href="/" class="header__logo">Y4T6 Project</a>
+            <div class="header__description">
+                <div class="header__description-row">the</div>
+                <div class="header__description-row">new way</div>
+                <div class="header__description-row">of everything</div>
+            </div>
+        </div>
+        <div class="header__column">
+            <?=$this->component('nav')?>
+        </div>
+        <div class="header__column">
+            <span class="header__phone-button">+38(095)134-33-38</span>
+        </div>
     </div>
 </header>
---
+++ app/components/nav/nav.php
@@ -0,0 +1,22 @@
+<nav class="nav">
+    <ul class="nav__list">
+        <li class="nav__list-item">
+            <a href="/" class="nav__item-link">Catalog</a>
+        </li>
+        <li class="nav__list-item">
+            <a href="/" class="nav__item-link">Services</a>
+        </li>
+        <li class="nav__list-item">
+            <a href="/" class="nav__item-link">About</a>
+        </li>
+        <li class="nav__list-item">
+            <a href="/" class="nav__item-link">Contacts</a>
+        </li>
+        <li class="nav__list-item">
+            <a href="https://instagram.com/y4t6project" target="_blank" class="nav__item-link nav__item-link--icon-type">
+                <span class="nav__item-main-icon"><?=icon('instagram')?></span>
+                <span class="nav__item-top-right-icon"><?=icon('arrow-top-right')?></span>
+            </a>
+        </li>
+    </ul>
+</nav>
